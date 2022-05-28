ecc92e8e3a7ebf56314beaee25a19183d81abeb4
Crazelu <eberelucky64@gmail.com>
2022-05-28 02:52:21 +0100

    chore: updates asset paths in example

--- example/main.dart
+++ example/main.dart
@@ -5,16 +5,16 @@ void main() async {
   const ENCRYPTION_KEY = "heheheehehhehe";
 
   final bytes = await Steganograph.encode(
-    image: File('assets/dogs.jpeg'),
+    image: File('example/assets/dogs.jpeg'),
     message: "Maybe I'm not ok!",
     encryptionKey: ENCRYPTION_KEY,
   );
 
-  final file = File('assets/result.png');
+  final file = File('example/assets/result.png');
   await file.writeAsBytes(bytes ?? []);
 
   final embeddedMessage = await Steganograph.decode(
-    image: File('assets/result.png'),
+    image: File('example/assets/result.png'),
     encryptionKey: ENCRYPTION_KEY,
   );
 
